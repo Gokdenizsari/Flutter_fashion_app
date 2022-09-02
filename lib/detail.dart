15cb29b783353c33bc8141e02dee165ff1d8618c
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-09-02 22:57:05 +0300

    Update detail.dart

--- lib/detail.dart
+++ lib/detail.dart
@@ -13,15 +13,55 @@ class _DetailState extends State<Detail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Hero(
-        tag: widget.imgPath,
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(widget.imgPath), fit: BoxFit.cover)),
+      body: Stack(
+     children: [Hero(
+          tag: widget.imgPath,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(widget.imgPath), fit: BoxFit.cover)),
+          ),
         ),
+        Positioned(
+          left: 15,
+          right: 15,
+          bottom: 15,
+          child: Material(
+            borderRadius: BorderRadius.circular(10),
+            elevation: 4,
+            child: Container(
+              height: 200,
+              width: MediaQuery.of(context).size.width - 30,//sağdan soldan 15 verdiğimiz için toplamı 30
+            
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(14.0),
+                      child: Container(
+                        height: 130,
+                        width: 75,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(color: Colors.grey),
+                        image: DecorationImage(image: AssetImage("assets/dress.jpg"),fit:BoxFit.contain),
+                        ),
+                        
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+            ),
+            
+          ),
+        ),
+        ],
       ),
     );
   }
