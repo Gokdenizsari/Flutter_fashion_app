c61dcf7a957db1af650982e49abad9bef3e7f573
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-09-02 23:13:06 +0300

    Update detail.dart

--- lib/detail.dart
+++ lib/detail.dart
@@ -14,53 +14,118 @@ class _DetailState extends State<Detail> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-     children: [Hero(
-          tag: widget.imgPath,
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(widget.imgPath), fit: BoxFit.cover)),
-          ),
-        ),
-        Positioned(
-          left: 15,
-          right: 15,
-          bottom: 15,
-          child: Material(
-            borderRadius: BorderRadius.circular(10),
-            elevation: 4,
+        children: [
+          Hero(
+            tag: widget.imgPath,
             child: Container(
-              height: 200,
-              width: MediaQuery.of(context).size.width - 30,//sağdan soldan 15 verdiğimiz için toplamı 30
-            
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
-            child: Column(
-              children: [
-                Row(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(widget.imgPath), fit: BoxFit.cover)),
+            ),
+          ),
+          Positioned(
+            left: 15,
+            right: 15,
+            bottom: 15,
+            child: Material(
+              borderRadius: BorderRadius.circular(10),
+              elevation: 4,
+              child: Container(
+                height: 250,
+                width: MediaQuery.of(context).size.width -
+                    30, //sağdan soldan 15 verdiğimiz için toplamı 30
+
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                child: Column(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.all(14.0),
-                      child: Container(
-                        height: 130,
-                        width: 75,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(color: Colors.grey),
-                        image: DecorationImage(image: AssetImage("assets/dress.jpg"),fit:BoxFit.contain),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(14.0),
+                          child: Container(
+                            height: 130,
+                            width: 75,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(color: Colors.grey),
+                              image: DecorationImage(
+                                  image: AssetImage("assets/dress.jpg"),
+                                  fit: BoxFit.contain),
+                            ),
+                          ),
                         ),
-                        
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "LAMINATED",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontFamily: "Montserrat",
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "Louis vuitton",
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontFamily: "Montserrat",
+                                  fontWeight: FontWeight.w300,
+                                  color: Colors.grey),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width - 180,
+                              height: 30,
+                              child: Text(
+                                "One button V-neck sling long-sleeved waist female stitching dress",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontFamily: "Montserrat",
+                                    fontWeight: FontWeight.w200),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Divider(),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 15, top: 11, bottom: 3),
+                      child: Row(
+                        mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "\$6500",
+                            style: TextStyle(
+                                fontSize: 22,
+                                color: Colors.black,
+                                fontFamily: "Montserrat"),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 20),
+                            child: FloatingActionButton(
+                              onPressed: () {},
+                              child: Center(child: Icon(Icons.arrow_back)),
+                              backgroundColor: Colors.brown,
+                            ),
+                          )
+                        ],
                       ),
                     )
                   ],
-                )
-              ],
+                ),
+              ),
             ),
-            ),
-            
           ),
-        ),
         ],
       ),
     );
